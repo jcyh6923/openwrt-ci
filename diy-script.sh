36dcd4f86bf46d6f1614b6a60fdfef6e856b7652
Blon Cic <59249447+jcyh6923@users.noreply.github.com>
2025-01-11 11:47:37 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -44,10 +44,10 @@ git clone https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lit
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 
 # 科学上网插件
-#git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # Themes
