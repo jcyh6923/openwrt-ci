0ff42996a92f3b9f6d2947637928d3e8dca8d3e0
Mr fa <59249447+jcyh6923@users.noreply.github.com>
2026-06-01 11:46:08 +0800

    Add Design theme to IPQ60XX custom services

--- libwrt-custom-services.sh
+++ libwrt-custom-services.sh
@@ -34,9 +34,10 @@ clone_pkg https://github.com/vernesong/OpenClash.git package/openclash master
 # DDNS-GO LuCI and ddns-go backend.
 clone_pkg https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go main
 
-# Nikki, Argon theme, and XUPNPD LuCI app.
+# Nikki, themes, and XUPNPD LuCI app.
 clone_pkg https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki main
 clone_pkg https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon master
+clone_pkg https://github.com/0x676e67/luci-theme-design.git package/luci-theme-design main
 clone_pkg https://github.com/jarod360/luci-app-xupnpd.git package/luci-app-xupnpd main
 rm -f package/luci-app-xupnpd/root/etc/init.d/xupnpd
 find package/luci-app-xupnpd -path '*/etc/init.d/xupnpd' -type f -delete
